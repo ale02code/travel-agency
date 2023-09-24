7c6f0939fe2f56dae6f2ebaab3a000deb25b0792
Christopher <114704469+Blade-24@users.noreply.github.com>
2023-09-24 14:05:11 -0600

    Delete commets in files javaScript

--- scripts.js/observer-video.js
+++ scripts.js/observer-video.js
@@ -7,10 +7,8 @@ observerAPI.observe(videoAboutUs);
 function verifyVisibility(entries) {
   let entry = entries[0];
   if (entry.isIntersecting) {
-    // if the element is visible
     videoAboutUs.setAttribute("autoplay", "on");
   } else {
-    // if the element is don't visible
     videoAboutUs.setAttribute("autoplay", "off");
   }
 }
