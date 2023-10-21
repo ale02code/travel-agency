06439997da231ad43a552ed3cf999f366f100b4c
Christopher <114704469+Blade-24@users.noreply.github.com>
2023-10-20 19:10:30 -0600

    fix nav bar

--- scripts.js/nav-bar-scroll.js
+++ scripts.js/nav-bar-scroll.js
@@ -3,8 +3,6 @@
 let mainLocation = window.scrollY;
 let nav = document.querySelector(".container-nav");
 let efectNavbar = document.querySelector(".show-menu-file");
-const menu = document.getElementById('menu');
-let menuOpen = menu.style.display = 'flex';
 
 window.addEventListener("scroll", function () {
   let currentLocation = window.scrollY;
