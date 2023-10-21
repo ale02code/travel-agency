966b4ca3dbe45d98284352317ccf5f4a7fefa898
Christopher <114704469+Blade-24@users.noreply.github.com>
2023-10-20 22:07:19 -0600

    resolution tablet

--- scripts.js/nav-bar-phone.js
+++ scripts.js/nav-bar-phone.js
@@ -4,17 +4,34 @@ const openMenu = document.getElementById("open-navbar");
 const closeMenu = document.getElementById("close-navbar");
 const menu = document.getElementById("menu");
 const containerLogo = document.getElementById("container-logo");
+const itemNavClick = document.querySelectorAll('.item-hover-close');
+const containerNav = document.querySelector('.container-nav');
 
-openMenu.addEventListener("click", function () {
+const openMenuFunct = () => {
+  containerNav.style.transition = '0s'
   menu.style.display = "flex";
   closeMenu.style.display = "block"
   openMenu.style.display = "none";
   containerLogo.style.display = "none"
-})
+}
 
-closeMenu.addEventListener("click", function () {
+const closeMenuFunct = () => {
   menu.style.display = "none";
   openMenu.style.display = "block";
   closeMenu.style.display = "none";
   containerLogo.style.display = "flex"
+}
+
+openMenu.addEventListener("click", function (e) {
+  openMenuFunct()
+})
+
+closeMenu.addEventListener("click", function () {
+  closeMenuFunct()
+})
+
+itemNavClick.forEach(item => {
+  item.addEventListener('click', function () {
+    closeMenuFunct()
+  })
 })
