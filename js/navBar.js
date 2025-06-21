648a353fb00977e957db1e09e30057962fdc5cc6
Christopher <114704469+Blade-24@users.noreply.github.com>
2025-06-21 09:28:11 -0600

    refactor: update navigation menu functionality and improve event handling for mobile responsiveness

--- js/navBar.js
+++ js/navBar.js
@@ -1,44 +1,50 @@
 "use strict"
 
-const openMenu = document.getElementById("open-navbar");
-const closeMenu = document.getElementById("close-navbar");
+const openMenuIcon = document.getElementById("open-navbar");
+const closeMenuIcon = document.getElementById("close-navbar");
 const menu = document.getElementById("menu");
 const containerLogo = document.getElementById("container-logo");
-const itemNavClick = document.querySelectorAll('.nav__menu-item');
+const itemNavClick = document.querySelectorAll('.nav__menu-item-a');
 const containerNav = document.querySelector('.header__nav-container');
 const nav = document.querySelector('.nav');
 
-const openMenuFunct = () => {
+let isMobile = () => window.innerWidth <= 1020;
+
+const openMenu = () => {
   containerNav.style.transition = '0s'
   menu.style.display = "flex";
-  closeMenu.style.display = "block"
-  openMenu.style.display = "none";
+  closeMenuIcon.style.display = "block"
+  openMenuIcon.style.display = "none";
   containerLogo.style.display = "none"
   nav.style.justifyContent = "center";
 }
 
-const closeMenuFunct = () => {
+const closeMenu = () => {
   menu.style.display = "none";
-  openMenu.style.display = "block";
-  closeMenu.style.display = "none";
+  openMenuIcon.style.display = "block";
+  closeMenuIcon.style.display = "none";
   containerLogo.style.display = "flex"
+  nav.style.justifyContent = "space-between";
 }
 
-openMenu.addEventListener("click", function (e) {
-  openMenuFunct();
+openMenuIcon.addEventListener("click", function (e) {
+  openMenu();
 })
 
-closeMenu.addEventListener("click", function (e) {
-  closeMenuFunct();
+closeMenuIcon.addEventListener("click", function (e) {
+  closeMenu();
 })
 
-// check width of screen
-let width = window.innerWidth;
-
-if (width <= 1020) {
+if (isMobile()) {
   itemNavClick.forEach(item => {
     item.addEventListener('click', function () {
-      closeMenuFunct()
+      closeMenu();
     })
   })
 }
+
+window.addEventListener('resize', function () {
+  if (isMobile()) {
+    closeMenu();
+  }
+})
